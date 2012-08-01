a8633ba01d3edab579c14d0c26bbcc8c630c1d4f
astrix <super_mad_alex@hotmail.com>
2012-08-01 15:41:42 +1000

    test commit

--- src/com/anonymous/solar/client/GreetingServiceAsync.java
+++ src/com/anonymous/solar/client/GreetingServiceAsync.java
@@ -3,6 +3,7 @@ package com.anonymous.solar.client;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 //sdfsdf
+//dabba be dabba die
 /**
  * The async counterpart of <code>GreetingService</code>.
  */
