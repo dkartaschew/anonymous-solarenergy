1adb7bf5270f4d89ce229fb2fda2cb22ea7aee48
Omega711 <minime_luke@hotmail.com>
2012-08-29 09:28:35 +1000

    Created generic XML converter class

---
+++ src/com/anonymous/solar/server/XMLConverter.java
@@ -0,0 +1,53 @@
+package com.anonymous.solar.server;
+
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ * 
+ * This class is a generic XML converter, that convert objects to XML files and XML files back
+ * to their respective Objects.
+ * 
+ * @author 07377495 Luke Durkan
+ * @version 1.0
+ *
+ */
+public class XMLConverter {
+
+	public XMLConverter() {
+		
+	}
+	
+	public <T> File convertObjectToXML(Object object, Class<T> cl) {
+		File file = new File("C:\\file.xml");
+		
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(cl);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+	 
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			jaxbMarshaller.marshal(object, file);
+		
+		} catch (JAXBException e) {
+			
+		}
+		
+		return file;
+	}
+	
+	public <T> Object convertXMLToObject(File file, Class<T> cl) {
+		Object returnObject = null;
+		
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(cl);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			returnObject = (Object) jaxbUnmarshaller.unmarshal(file);
+		} catch (JAXBException e) {
+		}
+		
+		return returnObject;
+	}
+}
