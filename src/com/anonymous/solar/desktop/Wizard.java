fa9eb82b12deb1124d0c7adcf2739eb10b5a0541
Thura <Thura@192.168.1.1>
2012-09-02 23:26:10 +1000

    Restored continutation restrictions

--- src/com/anonymous/solar/desktop/Wizard.java
+++ src/com/anonymous/solar/desktop/Wizard.java
@@ -205,7 +205,7 @@ public class Wizard extends javax.swing.JPanel {
 		if (wizardIndex + 1 < panels.size()) {
 			boolean changePanel = false;
 			try {
-				changePanel = ((WizardPanel) panels.get(wizardIndex)).callbackDispose(false);
+				changePanel = ((WizardPanel) panels.get(wizardIndex)).callbackDispose(true);
 			} catch (Exception e) {
 			}
 			if (changePanel) {
