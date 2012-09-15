259709b5387202c75e7e1b0b2307455a9b35ce77
Thura <Thura@Thura-PC>
2012-09-15 15:35:15 +1000

    Final comparison classes done.

---
+++ src/com/anonymous/solar/comparison/InverterEfficiencyComparison.java
@@ -0,0 +1,22 @@
+package com.anonymous.solar.comparison;
+
+import java.util.Comparator;
+import com.anonymous.solar.shared.SolarInverter;
+
+public class InverterEfficiencyComparison implements Comparator<SolarInverter> {
+
+	@Override
+	public int compare(SolarInverter inv1, SolarInverter inv2) {
+		Double cost1 = inv1.getInverterCost();
+		Double cost2 = inv2.getInverterCost();
+ 
+        if (cost1 > cost2){
+            return +1;
+        }else if (cost1 < cost2){
+            return -1;
+        }else{
+            return 0;
+        }
+	}
+
+}
---
+++ src/com/anonymous/solar/comparison/InverterLifeComparison.java
@@ -0,0 +1,22 @@
+package com.anonymous.solar.comparison;
+
+import java.util.Comparator;
+import com.anonymous.solar.shared.SolarInverter;
+
+public class InverterLifeComparison implements Comparator<SolarInverter> {
+
+	@Override
+	public int compare(SolarInverter inv1, SolarInverter inv2) {
+		Double item1 = inv1.getInverterLossYear();
+		Double item2 = inv2.getInverterLossYear();
+ 
+        if (item1 > item2){
+            return +1;
+        }else if (item1 < item2){
+            return -1;
+        }else{
+            return 0;
+        }
+	}
+
+}
