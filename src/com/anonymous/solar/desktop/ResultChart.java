4d3cbabebafd600c63d40abe34940f50e5dd0a83
Omega711 <minime_luke@hotmail.com>
2012-09-19 09:28:27 +1000

    Updated BurndownChart2, updated documentation for ResultChart

--- src/com/anonymous/solar/desktop/ResultChart.java
+++ src/com/anonymous/solar/desktop/ResultChart.java
@@ -14,6 +14,13 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 import com.anonymous.solar.shared.SolarResult;
 
+/**
+ * This class is designed to create an updatable JFreeChart object for displaying various
+ * data from the SolarResult class
+ * 
+ * @author 07377495 Luke Durkan
+ * @version 1.0
+ */
 public class ResultChart{
 	
 	private SolarResult solarResult;
@@ -26,15 +33,31 @@ public class ResultChart{
 
 	}
 	
+	/**
+	 * This method updates ResultChart with the data from the new Solar Result object
+	 * and recreates the JFreeChart object
+	 * 
+	 * @param newSolarResult the solarResult object to create a chart for
+	 */
 	public void updateChart(SolarResult newSolarResult) {
 		this.solarResult = newSolarResult;
 		setUpChart();
 	}
 	
+	/**
+	 * This method retrievs the JFreechart panel created by this class
+	 * 
+	 * @return a JFreeChart that gives a graphical view at some of the data calculated
+	 */
 	public JFreeChart getChartPanel() {
 		return resultChart;
 	}
 	
+	/**
+	 * This method creates the XYDataset used for the creation of the graph
+	 * 
+	 * @return the XYDataset to be used for the creation of the chart
+	 */
 	private XYDataset updateDataset() {
 		
 		XYSeriesCollection dataset = new XYSeriesCollection();
@@ -53,6 +76,9 @@ public class ResultChart{
 		return dataset;
 	}
 	
+	/**
+	 * This method creates the JFreeChart object and customises it as need be
+	 */
 	private void createChart() {
 		resultChart = ChartFactory.createXYLineChart(
 				"Calculation Results", 
@@ -73,6 +99,9 @@ public class ResultChart{
 		
 	}
 	
+	/**
+	 * This method runs the method required for creating a chart
+	 */
 	private void setUpChart() {
 		resultDataset = updateDataset();
 		createChart();
