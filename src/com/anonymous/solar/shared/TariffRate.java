3b465ea779f9772ec306406132046b31d584fdb2
Thura <Thura@192.168.1.1>
2012-09-21 10:35:24 +1000

    Updated tariff spreadsheet; Started tariff Class

---
+++ src/com/anonymous/solar/shared/TariffRate.java
@@ -0,0 +1,15 @@
+package com.anonymous.solar.shared;
+
+public class TariffRate {
+	
+	String provider;
+	double feedInFee;
+	double tariff11Fee;
+	double tariff13Fee;
+	double tariff12Fee;
+	double tariff31Fee;
+	double tariff33Fee;
+	
+	
+
+}
